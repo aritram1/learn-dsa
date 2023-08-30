1089be7816372901da23f42cd37ac1870d889f64
aritram1 <aritram1@gmail.com>
2023-08-31 04:20:03 +0530

    Update sorting.js

--- a/sorting/sorting.js
+++ a/sorting/sorting.js
@@ -12,26 +12,30 @@ let sortMethod = function(inp, f, debug=false){
             total++;
             if(debug) console.log(`after k=${k}, i=${i}th run for size ${inp.length} : `, inp);
         }
-        // if(debug) console.log(`after k=${k}th run :`, inp);
+        if(debug) console.log(`after k=${k}th run :`, inp);
     }
     return {'data' : inp, 'swapped' : swapped, 'total' : total};
 }
 
 // Driver function
-console.clear();
-let sample_size = 20;
-let result = [];
-for(let max=2; max<=sample_size; max++){
-    let input = Array.from({'length' : max}, (v,index)=> parseFloat((100*Math.random()).toFixed(2)));
-    console.log(`input :`, input);
-    let _sortFn = (a, b) => a > b;
-    let sorted = sortMethod(input, _sortFn, true);
-    let efficiency = `${(100*sorted.swapped/sorted.total).toFixed(2)}%`;      
-    console.log('Sorted Array :', sorted.data);
-    console.log('swapped :', sorted.swapped);
-    console.log('total', sorted.total);
-    
-    console.log(`Efficiency (Swapped/Total) : ${efficiency}`);
-    result.push(efficiency);
-}
-console.log(result);
+let driver = function(debug = false){
+    console.clear();
+    let sample_size = 20;
+    let result = [];
+    for(let max=5; max<=sample_size; max++){
+        let input = Array.from({'length' : max}, (v,index)=> parseFloat((100*Math.random()).toFixed(2)));
+        if(debug) console.log(`input :`, input);
+        let _sortFn = (a, b) => a > b;
+        let sorted = sortMethod(input, _sortFn, debug);
+        let efficiency = parseFloat(`${(100*sorted.swapped/sorted.total).toFixed(0)}`);      
+        if(debug) console.log('Sorted Array :', sorted.data);
+        if(debug) console.log('swapped :', sorted.swapped);
+        if(debug) console.log('total', sorted.total);
+        if(debug) console.log(`Efficiency (Swapped/Total) : ${efficiency}%`);
+        result.push(efficiency);
+    }
+    let sorted = result.sort();
+    console.log(`Avg. Efficiency : ${sorted[0]}%-${sorted[sorted.length-1]}%`);
+};
+
+driver();
